4aab814fa663c56e6d16dbc33df6def59282a580
Sam Neale <sammneale@icloud.com>
2021-10-23 13:29:41 +1100

    starting message

--- src/fmc.js
+++ src/fmc.js
@@ -18,12 +18,14 @@ function player(x) {
 function buttonClick(type, value){
     //Play Sound
     player('click');
+    console.log(type);
+    console.log(value);
     if(type == "function"){
 
-    }else if(type == "dButtonCol1"){
-
-    }else if(type == "dButtonCol1"){
-
+    }else if(type == "function-left"){
+        display.sideButtons.left[`btn${value + 1}`].func();
+    }else if (type == "function-right"){
+        display.sideButtons.right[`btn${value + 1}`].func();
     }else if(type == "num"){
 
     }else if(type == "char"){
@@ -37,39 +39,39 @@ let display = {
     sideButtons: {
         left:{
             btn1:{
-                desc: "TEST",
+                desc: "Welcome to IFFMC",
                 func: () =>{
 
                 }
             },
             btn2: {
-                desc: "TEST",
+                desc: "We were unable to connect to IF",
                 func: () => {
 
                 }
             },
             btn3: {
-                desc: "TEST",
+                desc: "Please check that IF is running",
                 func: () => {
 
                 }
             },
             btn4: {
-                desc: "TEST",
+                desc: "And the connect API is on.",
                 func: () => {
 
                 }
             },
             btn5: {
-                desc: "TEST",
+                desc: "Then reload the app:",
                 func: () => {
 
                 }
             },
             btn6: {
-                desc: "TEST",
+                desc: "CLICK TO RELOAD",
                 func: () => {
-
+                    window.location.reload();
                 }
             }
         },
@@ -105,9 +107,9 @@ let display = {
                 }
             },
             btn6: {
-                desc: "REALLLLLLY LONGN TEXT",
+                desc: "TEST",
                 func: () => {
-
+                    
                 }
             }
         }
@@ -136,11 +138,11 @@ function render(){
         // BTN 3
         displayCTX.fillText(display.sideButtons.left.btn3.desc, 20, 335);
         // BTN 4
-        displayCTX.fillText(display.sideButtons.left.btn4.desc, 20, 450);
+        displayCTX.fillText(display.sideButtons.left.btn4.desc, 20, 445);
         // BTN 5
         displayCTX.fillText(display.sideButtons.left.btn5.desc, 20, 560);
         // BTN 6
-        displayCTX.fillText(display.sideButtons.left.btn6.desc, 20, 675);
+        displayCTX.fillText(display.sideButtons.left.btn6.desc, 20, 670);
     //Right Side
     displayCTX.textAlign = 'right';
         // BTN 1
@@ -150,13 +152,12 @@ function render(){
         // BTN 3
         displayCTX.fillText(display.sideButtons.right.btn3.desc, 980, 335);
         // BTN 4
-        displayCTX.fillText(display.sideButtons.right.btn4.desc, 980, 450);
+        displayCTX.fillText(display.sideButtons.right.btn4.desc, 980, 445);
         // BTN 5
         displayCTX.fillText(display.sideButtons.right.btn5.desc, 980, 560);
         // BTN 6
-        displayCTX.fillText(display.sideButtons.right.btn6.desc, 980, 675);
+        displayCTX.fillText(display.sideButtons.right.btn6.desc, 980, 670);
 
 }
 
 setInterval(render, 100);
-display.sideButtons.left.btn1.desc = "HELLO"
