f0310e82d2e8dc130a307849d510bd634fce1973
Sam Neale <sammneale@icloud.com>
2021-11-20 22:44:31 +1100

    Ignore control

--- src/fmc.js
+++ src/fmc.js
@@ -1198,6 +1198,7 @@ window.addEventListener('keydown', (e) => {
             case "META":
             case "DEAD":
             case "ALT":
+            case "CONTROL":
             case "SHIFT":
                 break;
             case " ":
